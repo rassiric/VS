770f965611bbe2e897ba2886027ce0fedd856be4
Ramiz <ramesbahrami@gmail.com>
2016-05-30 16:52:01 +0200

    You are mine now! You belong to me!
    Benchmark Version!

--- mat/src/main.rs
+++ mat/src/main.rs
@@ -1,50 +1,24 @@
-use std::io::prelude::*;
 use std::net::*;
-use std::io::stdin;
 use std::str::FromStr;
 
 const MATID : u8 = 0;
 
 fn main() {
-    let mut level = 10;
+    let mut streams = Vec::<UdpSocket>::new();
     let addr = SocketAddrV4::from_str("127.0.0.1:18000").unwrap();
-    let stream = UdpSocket::bind("0.0.0.0:0").unwrap(); //Bind to any IP, let OS choose port
-    stream.send_to(&[(2+MATID)], &addr).unwrap(); //Register as material
 
-    let mut input = String::new();
-    loop{
-        let mut usebuf = [0];
-        match stream.recv_from(&mut usebuf) {
-            Ok((_, source)) => {
-                match source {
-                    SocketAddr::V4(sourceaddr) => {
-                        if sourceaddr != addr {
-                            println!("Something is running interference!");
-                            continue;
-                        }
-                    },
-                    SocketAddr::V6(_) => {
-                        println!("Something is running interference!");
-                        continue;
-                    }
-                }
+    let mut i = 1;
 
+    loop {
+        let stream = match UdpSocket::bind("0.0.0.0:0") {
+            Err(e) => {
+                println!("Error while opening {}. socket: {}", i, e);
+                return
             },
-            Err(err) => {
-                unreachable!("Error while reading used material: {}", err);
-            }
-        }
-        level -= usebuf[0]; //material abziehen
-        println!("Matlevel: {}", level);
-        if level > 2 {
-            continue;
-        }
-        println!("Nearly empty, halting!", );
-
-        let _ = stream.send_to(&[255], addr); //notify nearly empty
-        stdin().read_line(&mut input).unwrap(); //wait till enter to reset
-        level = 20;
-        let _ = stream.send_to(&[1], addr); //notify nearly empty
+            Ok(s) => s
+        };
+        i+=1;
+        stream.send_to(&[(2+MATID)], &addr).unwrap(); //Register as material
+        streams.push(stream);
     }
-
 }
